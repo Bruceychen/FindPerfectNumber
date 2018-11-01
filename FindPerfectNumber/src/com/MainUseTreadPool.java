df0e0d3cee9dec10b3d5d1742b1f17bfc208defe
BruceyChenWhite <r08006@gmail.com>
2018-11-01 08:49:44 +0800

    use system.nanoTime for check testing time; add more error handling statements for thread terminate steps

--- FindPerfectNumber/src/com/MainUseTreadPool.java
+++ FindPerfectNumber/src/com/MainUseTreadPool.java
@@ -10,51 +10,61 @@ public class MainUseTreadPool {
 
     private static List<Integer> resultList = new ArrayList<>();
 
-    private static int interval = 2000000;
-//    private static int interval = 30;
-//        private static int max = 34000000;
-    private static int max = 8000000;
+    //    private static int interval = 2000000;
+    private static int interval = 30;
+    //        private static int max = 34000000;
+    private static int max = 5000000;
 
     public static synchronized void addToList(final int number) {
         resultList.add(number);
     }
-    
-    
+
     public static void main(final String[] args) throws InterruptedException {
 
         final int availableProcessors = Runtime.getRuntime().availableProcessors();
-        
+
         // 不想用100% cpu跑
-        final int usedProcessors =availableProcessors -2;
-        
+        final int usedProcessors = availableProcessors - 0;
+
         System.out.println("我可以使用的邏輯處理器" + Runtime.getRuntime().availableProcessors());
-        System.out.println("這次使用"+ usedProcessors +"個邏輯處理器");
-         
-        final ExecutorService executor = Executors.newFixedThreadPool(usedProcessors); 
+        System.out.println("這次使用" + usedProcessors + "個邏輯處理器");
 
-        final long start = System.currentTimeMillis();
+        final ExecutorService executor = Executors.newFixedThreadPool(usedProcessors);
 
+        //        final long start = System.currentTimeMillis();
+        final long start = System.nanoTime();
+        System.out.println("Start at nanoTime: " + start);
         // Scene 1: assign 30 numbers for each thread a time.
         for (int i = 1; i < max; i += interval) {
-            final Worker worker = new Worker(i, i + (interval - 1));
+            final Worker worker = new Worker(i, i + interval - 1);
             executor.execute(worker);
         }
-        
+
         // Scene 2: run through
-//      for (int i = 1; i < max; i ++) {
-//      executor.submit(new Worker2(i));
-//  }
-      
+        //        for (int i = 1; i < max; i++) {
+        //            executor.submit(new Worker2(i));
+        //        }
+
         executor.shutdown();
 
-        if (executor.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS)) {
+        try {
+            if (!executor.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS)) {
+                executor.shutdown();
+                if (!executor.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS)) {
+                    System.err.println("Executor did not terminated");
+                }
+            }
+        } catch (final Exception e) {
+            executor.shutdown();
+            Thread.currentThread().interrupt();
+        } finally {
             System.out.println("main執行緒結束");
             System.out.println(resultList);
-            final long end = System.currentTimeMillis();
-
+            //            final long end = System.currentTimeMillis();
+            final long end = System.nanoTime();
+            System.out.println("End at nano time: " + end);
             System.out.println("結束應用");
-            System.out.println("共用了" + (end - start)/1000 + "秒");
+            System.out.println("共用了" + (end - start) + "nano秒");
         }
-
     }
 }
--- FindPerfectNumber/src/com/Worker2.java
+++ FindPerfectNumber/src/com/Worker2.java
@@ -1,17 +1,18 @@
 package com;
 
 public class Worker2 extends Thread {
-    private int number;
+    private final int number;
 
     public Worker2(final int number) {
         this.number = number;
     }
-    
+
     @Override
     public void run() {
-            if(Calculator.findPerfectNumber(number)) {
-                System.out.println(number + "是完美數字喔");
-                MainUseTreadPool.addToList(number);
-            }
+        //        System.out.println("Current thread: " + Thread.currentThread().getName() + ", validating number " + this.number);
+        if (Calculator.findPerfectNumber(this.number)) {
+            System.out.println(this.number + "是完美數字喔");
+            MainUseTreadPool.addToList(this.number);
+        }
     }
 }
